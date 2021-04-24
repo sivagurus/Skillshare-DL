14fa7962b05eadc2015821ee331ef314ca955c8c
calvinhobbes23 <71798238+calvinhobbes23@users.noreply.github.com>
2021-04-23 20:53:40 -0400

    Add files via upload

--- dl.py
+++ dl.py
@@ -1,11 +1,11 @@
 import sys, os
 from skillshare import Skillshare, splash
-from magic import cookie
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
+    cookie = sys.argv[2]
     dl = Skillshare(cookie)
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
